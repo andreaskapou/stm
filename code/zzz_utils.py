5b03dbd4e3870c43fc54eb1b2f54d09cee727bdb
Andrew Papanastasiou <Andrew Papanastasiou>
2022-12-07 11:54:39 +0000

    LDA simulation and associated QC updated to include vocab. Couple of plotting routines added.

--- code/zzz_utils.py
+++ code/zzz_utils.py
@@ -83,6 +83,11 @@ def simulate_lda_dataset(nTopics, nCells, nRegions, N, a, b):
 
     # D_tfidf is a matrix of tf-idf (rows are cells, columns are regions, elements are tf-idf values)
     #D_tfidf = TfidfTransformer().fit_transform(D_freq)
+
+    # add vocabulary (region <-> word dataframe)
+    region_idx   = list(range(nRegions))
+    region_words = ['w'+str(idx) for idx in region_idx]
+    vocab = pd.DataFrame(zip(region_words,region_idx),columns=['region_id','region_index'])
     
     obj = dict()
     obj['D'] = D
@@ -91,6 +96,7 @@ def simulate_lda_dataset(nTopics, nCells, nRegions, N, a, b):
     #obj['D_tfidf'] = D_tfidf
     obj['theta_true'] = theta
     obj['phi_true'] = phi
+    obj['vocab'] = vocab
     
     return obj
     
@@ -106,20 +112,73 @@ def scale_zero_one(x):
     x = (x - x_min) / (x_max - x_min) # Broadcasting rules apply
     return x
 
+#########################
+### plotting routines ###
+#########################
+
+def make_theta_comparison_plots(theta_true,theta_infer):
+    """
+    Quick plot to compare sets of theta values (presumably true vs inferred)
+    Inputs:
+    theta_true: array of theta values (true)
+    theta_infer: array of theta values (inferred)
+    """
+    
+    ntopics_true  = theta_true.shape[1]
+    ntopics_infer = theta_infer.shape[1] 
+    fig, axs = plt.subplots(ntopics_true,ntopics_infer,
+                            figsize=(ntopics_true*1.5,ntopics_true*1.5))
+    
+    for i in range(ntopics_true):
+        for j in range(ntopics_infer):
+            axs[i,j].plot(theta_true[:,i],theta_infer[:,j],'o',markersize=2,alpha=0.5)
+            axs[i,j].set_xlim(0,1)
+            axs[i,j].set_ylim(0,1)
+            axs[i,j].grid(True)
+            
+    
+    plt.tight_layout()
+    return fig
+
+
+# def plot_word_cloud(phi, vocab, max_ids, ax, title):
+#     """
+#     Word cloud visualisation helpful for interpreting topic-word distributions
+# 
+#     :param phi: Vector of word probabilities for specific topic
+#     :param vocab: Vocabulary array with columns ('index', 'id')
+#     :param max_ids: Maximum number of word ids to plot.
+#     :param ax: Axis object
+#     :param title: Plot title
+#     """
+#     sorted_, indices = torch.sort(phi, descending=True)
+#     df = pd.DataFrame(indices[:max_ids].numpy(), columns=['index'])
+#     words = pd.merge(df, vocab[['index', 'id']],
+#                      how='left', on='index')['id'].values.tolist()
+#     sizes = (sorted_[:100] * 10).int().numpy().tolist()
+#     freqs = {words[i]: sizes[i] for i in range(len(words))}
+#     wc = WordCloud(background_color="white", width=800, height=500)
+#     wc = wc.generate_from_frequencies(freqs)
+#     ax.set_title(title)
+#     ax.imshow(wc, interpolation='bilinear')
+#     ax.axis("off")
+
+
+
 def plot_word_cloud(phi, vocab, max_ids, ax, title):
     """
     Word cloud visualisation helpful for interpreting topic-word distributions
 
     :param phi: Vector of word probabilities for specific topic
-    :param vocab: Vocabulary array with columns ('index', 'id')
+    :param vocab: Vocabulary array with columns ('id', 'index')
     :param max_ids: Maximum number of word ids to plot.
     :param ax: Axis object
     :param title: Plot title
     """
     sorted_, indices = torch.sort(phi, descending=True)
-    df = pd.DataFrame(indices[:max_ids].numpy(), columns=['index'])
-    words = pd.merge(df, vocab[['index', 'id']],
-                     how='left', on='index')['id'].values.tolist()
+    df = pd.DataFrame(indices[:max_ids].numpy(), columns=['region_index'])
+    words = pd.merge(df, vocab[['region_index', 'region_id']],
+                     how='left', on='region_index')['region_id'].values.tolist()
     sizes = (sorted_[:100] * 10).int().numpy().tolist()
     freqs = {words[i]: sizes[i] for i in range(len(words))}
     wc = WordCloud(background_color="white", width=800, height=500)
@@ -127,3 +186,30 @@ def plot_word_cloud(phi, vocab, max_ids, ax, title):
     ax.set_title(title)
     ax.imshow(wc, interpolation='bilinear')
     ax.axis("off")
+    
+
+def make_word_cloud_figure(phi, vocab, max_ids):
+    """
+    Make grid of wordclouds to visualize topic-word distributions
+
+    :param phi: Vector of word probabilities for specific topic
+    :param vocab: Vocabulary array with columns ('id', 'index')
+    :param max_ids: Maximum number of word ids to plot.
+    """
+    
+    ntopics = phi.shape[0]
+    ncols = 4
+    nrows = int(np.floor(ntopics/4)) + 1
+    nremain = (ncols * nrows) - ntopics
+    
+    fig, axs = plt.subplots(nrows, ncols, figsize=(12, nrows * 3.))
+    axs = axs.ravel()
+    for n in range(ntopics):
+        plot_word_cloud(scale_zero_one(phi[n]), vocab, 
+                        max_ids, axs[n], 'Topic %d' % (n + 1))
+        
+    for j in range(nremain):
+        axs[-j-1].axis('off')
+
+    plt.tight_layout()
+    return fig
--- code/zzz_utils2.py
+++ code/zzz_utils2.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 def qc_D_entry(D_ent, qc_peak_idx, target_len):
     """
@@ -64,10 +66,15 @@ def qc_simulation_object(obj, peak_detection_qc_thr):
     for k, entry in enumerate(tmp_original_entries):
         original_qc_dict[entry] = k
 
-    # secoond, adjust entries of D matrix according to above dictionary
+    # second, adjust entries of D matrix according to above dictionary
     # for this trick using features of python dictionaries, 
     # see: https://stackoverflow.com/questions/16992713/translate-every-element-in-numpy-array-according-to-key
     obj_qc['D'] = np.vectorize(original_qc_dict.__getitem__)(obj_qc['D']) 
+
+    # finally adjust vocab dataframe
+    obj_qc_vocab = obj['vocab'].iloc[qc_peak_idx].copy(deep=True)
+    obj_qc_vocab.reset_index(drop=True, inplace=True)
+    obj_qc_vocab['region_index'] = obj_qc_vocab.index.tolist()
 
     print('shape  of D after QC:            ',obj_qc['D'].shape)
     print('length of D_freq after QC:       ',len(obj_qc['D_str']))
@@ -76,8 +83,87 @@ def qc_simulation_object(obj, peak_detection_qc_thr):
     obj_qc['theta_true'] = obj['theta_true']
     obj_qc['phi_true']   = obj['phi_true'][:,qc_peak_idx]
     obj_qc['phi_true_norm']   = (obj_qc['phi_true'].T / obj_qc['phi_true'].sum(axis=1)).T
+    obj_qc['vocab'] = obj_qc_vocab
     print('shape  of theta_true after QC:   ', obj_qc['theta_true'].shape)
     print('shape  of phi_true after QC:     ', obj_qc['phi_true'].shape)
     print('shape  of phi_true_norm after QC:',  obj_qc['phi_true_norm'].shape)
     
     return obj_qc
+
+
+#########################
+### plotting routines ###
+#########################
+
+# def make_theta_comparison_plots(theta_true,theta_infer):
+#     """
+#     Quick plot to compare sets of theta values (presumably true vs inferred)
+#     Inputs:
+#     theta_true: array of theta values (true)
+#     theta_infer: array of theta values (inferred)
+#     """
+#     
+#     ntopics_true  = theta_true.shape[1]
+#     ntopics_infer = theta_infer.shape[1] 
+#     fig, axs = plt.subplots(ntopics_true,ntopics_infer,
+#                             figsize=(ntopics_true*1.5,ntopics_true*1.5))
+#     
+#     for i in range(ntopics_true):
+#         for j in range(ntopics_infer):
+#             axs[i,j].plot(theta_true[:,i],theta_infer[:,j],'o',markersize=2,alpha=0.5)
+#             axs[i,j].set_xlim(0,1)
+#             axs[i,j].set_ylim(0,1)
+#             axs[i,j].grid(True)
+#             
+#     
+#     plt.tight_layout()
+#     return fig
+# 
+# 
+# def plot_word_cloud(phi, vocab, max_ids, ax, title):
+#     """
+#     Word cloud visualisation helpful for interpreting topic-word distributions
+# 
+#     :param phi: Vector of word probabilities for specific topic
+#     :param vocab: Vocabulary array with columns ('id', 'index')
+#     :param max_ids: Maximum number of word ids to plot.
+#     :param ax: Axis object
+#     :param title: Plot title
+#     """
+#     sorted_, indices = torch.sort(phi, descending=True)
+#     df = pd.DataFrame(indices[:max_ids].numpy(), columns=['region_index'])
+#     words = pd.merge(df, vocab[['region_index', 'region_id']],
+#                      how='left', on='region_index')['region_id'].values.tolist()
+#     sizes = (sorted_[:100] * 10).int().numpy().tolist()
+#     freqs = {words[i]: sizes[i] for i in range(len(words))}
+#     wc = WordCloud(background_color="white", width=800, height=500)
+#     wc = wc.generate_from_frequencies(freqs)
+#     ax.set_title(title)
+#     ax.imshow(wc, interpolation='bilinear')
+#     ax.axis("off")
+#     
+# def make_word_cloud_figure(phi, vocab, max_ids):
+#     """
+#     Make grid of wordclouds to visualize topic-word distributions
+# 
+#     :param phi: Vector of word probabilities for specific topic
+#     :param vocab: Vocabulary array with columns ('id', 'index')
+#     :param max_ids: Maximum number of word ids to plot.
+#     """
+#     
+#     ntopics = phi.shape[0]
+#     ncols = 4
+#     nrows = int(np.floor(ntopics/4)) + 1
+#     nremain = (ncols * nrows) - ntopics
+#     
+#     fig, axs = plt.subplots(nrows, ncols, figsize=(12, nrows * 3.))
+#     for n in range(ntopics):
+#         i, j = divmod(n, ncols)
+#         plot_word_cloud(scale_zero_one(phi[n]), vocab, 
+#                         max_ids, axs[i, j], 'Topic %d' % (n + 1))
+#         
+#     for j in range(nremain):
+#         axs[-1,-j-1].axis('off')
+# 
+#     plt.tight_layout()
+#     return fig
